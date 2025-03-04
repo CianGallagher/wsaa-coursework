c9fcdd5dd314c44593f08689f0a2451d30df1f91
Cian Gallagher <ciangall1@gmail.com>
2025-03-04 20:19:31 +0000

    replaced URL with content URL and output looks promising, still don't have access to the text though

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -2,12 +2,13 @@ import requests
 import json
 from config import apikeys as cfg
 
-url = 'https://api.github.com/repos/CianGallagher/PrivateRepo'
+# url = 'https://api.github.com/repos/CianGallagher/PrivateRepo'
+contents_url = "https://api.github.com/repos/CianGallagher/PrivateRepo/contents/andrew.txt"
 
 apikey = cfg['github_private_repo_key']
 
 # Request the data from the API using the URL and API key
-response = requests.get(url, auth=('token', apikey))
+response = requests.get(contents_url, auth=('token', apikey))
 
 print(response.status_code)
 
@@ -16,4 +17,4 @@ with open(filename, 'w') as f:
     json.dump(response.json(), f, indent=4)
 
 # Find a way to interact with the andrew.txt file within this repo.
-contents_url = "https://api.github.com/repos/CianGallagher/PrivateRepo/contents/{+path}"
+# Content in the JSON output is stated as "content": "QW5kcmV3Cg==\n", not sure what language this is.
